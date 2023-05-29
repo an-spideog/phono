85a0a14e1cb3a9e894840834a053ba4c3f19373a
killuaDev <louis.l.cullen@gmail.com>
2023-05-29 11:00:07 +0100

    Test to make sure .env files aren't committed

--- src/routes/introduction/+page.server.ts
+++ src/routes/introduction/+page.server.ts
@@ -1,23 +1,4 @@
-import mysql from 'mysql2/promise';
+import sql from 'mssql'; 
+import { FAKE_PASSWORD } from '$env/static/private';
 
-const con = await mysql.createConnection({
-    host: 'localhost',
-    user: 'admin',
-    password: 'toor',
-    database: 'phono_test'
-});
-
-console.log("Server code logs on the server console, that makes sense");
-
-let results = await con.query('SELECT * FROM spools')
-    .then(function([rows,fields]) { // Don't know what this does, but it seems to work
-            console.log(rows);
-            return rows;
-    });
-
-export function load() {
-    return {
-        reels: results
-    };
-}
-
+console.log(FAKE_PASSWORD);
